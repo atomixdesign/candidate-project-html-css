4b1200ceb22db1cf05ea1d2536786beafac89f8e
Mohamed Farique <mohammedfariq@gmail.com>
2022-08-15 13:11:33 +0530

    Navbar update 50% done

---
+++ src/scripts/main.js
@@ -0,0 +1,9 @@
+// Toggle Mobile Menu
+const open_menu = document.querySelector(".open-menu");
+const navbar = document.querySelector(".navbar");
+
+open_menu.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  navbar.classList.toggle("is-active");
+}
