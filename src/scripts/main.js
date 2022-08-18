feb96c7b56705f787fb8cbd4f50245ba50979bfa
Mohamed Farique <mohammedfariq@gmail.com>
2022-08-19 01:53:05 +0530

    Latest Uodate

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menuItems = document.querySelector(".menu__items");
 const expandBtn = document.querySelectorAll(".expand-btn");
-
 // humburger toggle
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("open__");
